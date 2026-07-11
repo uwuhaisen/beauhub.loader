bf14f502890488f72a558013acf15bc40602ecee
uwuhaisen <uwuhaisen@gmail.com>
2026-07-11 15:07:50 +0200

    Update beauhub.lua

--- beauhub.lua
+++ beauhub.lua
@@ -1,12 +1,13 @@
-local Games = {
-  [9561553764] = "https://raw.githubusercontent.com/uwuhaisen/beauhub.loader/refs/heads/main/Murder%20Duels", -- Murder Duel
-  [10186342197] = "https://raw.githubusercontent.com/uwuhaisen/beauhub.loader/refs/heads/main/Make%20Hotsauce", -- Make Hotsauce
-  [10209534490] = "https://raw.githubusercontent.com/uwuhaisen/beauhub.loader/refs/heads/main/Chicken%20Farm", -- Chicken Farm
-  [10081292501] = "https://raw.githubusercontent.com/uwuhaisen/beauhub.loader/refs/heads/main/Bomb%20Fishing", -- Bomb Fishing!
-  [10267363348] = "https://raw.githubusercontent.com/uwuhaisen/beauhub.loader/refs/heads/main/Drain%20The%20Lake", -- Drain the Lake
-  [10053091404] = "https://raw.githubusercontent.com/uwuhaisen/beauhub.loader/refs/heads/main/Buid%20A%20Pet%20Farm", -- Build a Pet Farm
-  [10199301628] = "https://raw.githubusercontent.com/uwuhaisen/beauhub.loader/refs/heads/main/Merge%20A%20Nuke", -- Merge a Nuke!
-  [10188443172] = "https://raw.githubusercontent.com/uwuhaisen/beauhub.loader/refs/heads/main/Merge%20Plant%20Vs%20Mobs", -- Merge Plants vs Mobs
-}
+--!strict
 
-return Games
+local HttpGet = game.HttpGet
+local GameId: number = game.GameId
+
+local Games: {[number]: string} = loadstring(
+  HttpGet(game, "https://raw.githubusercontent.com/AhmadV99/Speed-Hub-X/main/GameList.lua")
+)() :: any
+
+local URL: string? = Games[GameId]
+if not URL then return end
+
+loadstring(HttpGet(game, URL))()
